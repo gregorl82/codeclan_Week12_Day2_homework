01c3dd472816c408d2100767d37b1ca1985ced24
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-04 17:41:06 +0000

    Adds ThemePark class and checks for name, attractions and stalls

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,29 @@
+import attractions.Attraction;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private String name;
+    private ArrayList<Stall> stalls;
+    private ArrayList<Attraction> attractions;
+
+    public ThemePark(String name, ArrayList<Attraction> attractions, ArrayList<Stall> stalls){
+        this.name = name;
+        this.attractions = attractions;
+        this.stalls = stalls;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int countAttractions(){
+        return this.attractions.size();
+    }
+
+    public int countStalls(){
+        return this.stalls.size();
+    }
 }
---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,64 @@
+import attractions.*;
+import org.junit.Before;
+import org.junit.Test;
+import stalls.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class ThemeParkTest {
+
+    private ThemePark themePark;
+
+    private ArrayList<Stall> stalls;
+    private ArrayList<Attraction> attractions;
+    private Dodgems dodgems;
+    private Park park;
+    private Playground playground;
+    private RollerCoaster rollerCoaster;
+
+    private CandyflossStall candyflossStall;
+    private IceCreamStall iceCreamStall;
+    private TobaccoStall tobaccoStall;
+
+    @Before
+    public void before(){
+        dodgems = new Dodgems("Bumper Cars", 5);
+        park = new Park("Leafy Meadows", 9);
+        rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        playground = new Playground("Fun Zone", 7);
+        attractions = new ArrayList<>();
+        attractions.add(dodgems);
+        attractions.add(park);
+        attractions.add(rollerCoaster);
+        attractions.add(playground);
+
+        candyflossStall = new CandyflossStall("Candy Land", "Harry Belafonte", ParkingSpot.A1, 3);
+        iceCreamStall = new IceCreamStall("Dream Cones", "Vanilla Ice", ParkingSpot.A4, 4);
+        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1, 2);
+        stalls = new ArrayList<>();
+        stalls.add(candyflossStall);
+        stalls.add(iceCreamStall);
+        stalls.add(tobaccoStall);
+
+        themePark = new ThemePark("Fawlty Towers", attractions, stalls);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Fawlty Towers", themePark.getName());
+    }
+
+    @Test
+    public void hasAttractions(){
+        assertEquals(4, themePark.countAttractions());
+    }
+
+    @Test
+    public void hasStalls(){
+        assertEquals(3, themePark.countStalls());
+    }
+
+}
