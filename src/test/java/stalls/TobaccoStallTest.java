bcf050f4973eba4d1ea6d98daae1b61c74d93970
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-04 17:17:46 +0000

    Adds implementations of IReviewed interface in stalls and attractions

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -14,7 +14,7 @@ public class TobaccoStallTest {
 
     @Before
     public void setUp() throws Exception {
-        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1, 2);
         visitor1 = new Visitor(18, 159, 40.0);
         visitor2 = new Visitor(17, 171, 23.0);
     }
@@ -32,6 +32,11 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void hasRating() {
+        assertEquals(2, tobaccoStall.getRating());
     }
 
     @Test
